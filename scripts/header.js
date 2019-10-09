40be9cab90e599c49864b9c195003d4319282cdc
LTania <tania.luchynska@gmail.com>
2019-10-09 16:07:13 +0300

    some commit

---
+++ scripts/header.js
@@ -0,0 +1,25 @@
+function scrollHome() {
+    let goal = document.getElementsByClass('home')[0] ;
+    goal.scrollIntoView();
+}
+
+function scrollFractals() {
+    let goal = document.getElementsByClass('fractals')[0] ;
+    goal.scrollIntoView();
+}
+function scrollColors() {
+    let goal = document.getElementsByClass('colors')[0] ;
+    goal.scrollIntoView();
+}
+function scrollAffin() {
+    let goal = document.getElementsByClass('affin')[0] ;
+    goal.scrollIntoView();
+}
+function scrollAbout() {
+    let goal = document.getElementsByClass('info')[0] ;
+    goal.scrollIntoView();
+}
+function scrollTeam() {
+    let goal = document.getElementsByClass('team')[0] ;
+    goal.scrollIntoView();
+}
