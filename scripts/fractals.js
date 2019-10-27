e2ff9be7f0b8a36ddd5c9b8ca69c0155dce6e525
Bohdan <iabodia88@gmail.com>
2019-10-27 18:04:36 +0200

    Add Mandelbrot set and scopeing but work bad

---
+++ scripts/fractals.js
@@ -0,0 +1,67 @@
+function fractal(){
+    var xmin = -1.9, ymin = -1.5, scale = 200;
+        var x, y, i, xt;
+        var color;
+        var canvas = document.getElementById('myCanvas');
+        canvas.addEventListener("mousedown", zoom, false);
+        var context = canvas.getContext('2d');
+        mandel();
+        function zoom(event) {
+            xmin=xmin+event.pageX/4/scale;
+            ymin=-event.pageY/4/scale+200/scale+ymin;
+            scale=scale+300;
+            mandel();
+        }
+
+        function checkIfBelongsToMandelbrotSet(x, y) {
+            var realComponentOfResult = x;
+            var imaginaryComponentOfResult = y;
+            var maxIterations = 100;
+            for (var i = 0; i < maxIterations; i++) {
+                var tempRealComponent = realComponentOfResult * realComponentOfResult
+                    - imaginaryComponentOfResult * imaginaryComponentOfResult
+                    + x;
+                var tempImaginaryComponent = 2 * realComponentOfResult * imaginaryComponentOfResult
+                    + y;
+                realComponentOfResult = tempRealComponent;
+                imaginaryComponentOfResult = tempImaginaryComponent;
+
+                // Return a number as a percentage
+                if (Math.abs(realComponentOfResult + imaginaryComponentOfResult) > 16)
+                    return (i / maxIterations * 100);
+            }
+            return 0;   // Return zero if in set        
+        }
+
+        function mandel() {
+            for (x = 0; x < canvas.width; x++) {
+                for (y = 0; y < canvas.height; y++) {
+                    i = 0;
+                    cx = xmin + x / scale;
+                    cy = ymin + y / scale;
+                    var belongsToSet =
+							checkIfBelongsToMandelbrotSet(x / scale + xmin,
+								y / scale + ymin);
+                    // zx = 0;
+                    // zy = 0;
+
+                    // do {
+                    //     xt = zx * zy;
+                    //     zx = zx * zx - zy * zy + cx;
+                    //     zy = 2 * xt + cy;
+                    //     i++;
+                    // }
+                    // while (i < 255 && (zx * zx + zy * zy) < 16);
+
+                    if (belongsToSet == 0) {
+                        context .fillStyle = '#000';
+							context .fillRect(x, y, 1, 1); // Draw a black pixel
+						} else {
+							context .fillStyle = 'hsl(0, 100%, ' + belongsToSet + '%)';
+							context .fillRect(x, y, 1, 1); // Draw a colorful pixel
+						}
+                }
+            }
+        }
+}
+
