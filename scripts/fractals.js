2a337169e16d343b3bd6e94f575e7b615f38b522
Bohdan <iabodia88@gmail.com>
2019-11-03 21:28:36 +0200

    Fix scale in Julia set

--- scripts/fractals.js
+++ scripts/fractals.js
@@ -1,94 +1,99 @@
-function fractal(){
-        var canvas = document.getElementById('fractalCanvas');
-        canvas.addEventListener('mousedown', zoom, false);
-        var context = canvas.getContext('2d');
-        var xmin, ymin, scale = 200;
-        var x, y;
+function fractal() {
+    var canvas = document.getElementById('fractalCanvas');
+    canvas.addEventListener('mousedown', zoom, false);
+    var context = canvas.getContext('2d');
+    var xmin, ymin, scale = 200;
+    var x, y;
 
-        var isJulia;
-        var constForJuliaX = document.getElementById('constForJuliaX').value;
-        var constForJuliaY = document.getElementById('constForJuliaY').value;
-        console.log(constForJuliaX, constForJuliaY);
+    var isJulia;
+    var constForJuliaX = document.getElementById('constForJuliaX').value;
+    var constForJuliaY = document.getElementById('constForJuliaY').value;
+    console.log(constForJuliaX, constForJuliaY);
 
-        var fractalRadioButt = document.getElementsByName('fractalChoose');
-        var fractalChoose;
-        for(var i = 0; i < fractalRadioButt.length; i++){
-            if(fractalRadioButt[i].checked) 
-               fractalChoose = fractalRadioButt[i].value;
-        }
+    var fractalRadioButt = document.getElementsByName('fractalChoose');
+    var fractalChoose;
+    for (var i = 0; i < fractalRadioButt.length; i++) {
+        if (fractalRadioButt[i].checked)
+            fractalChoose = fractalRadioButt[i].value;
+    }
 
-        if(fractalChoose == 'mandelbrotSet'){
-            xmin = -1.9, ymin = -1.5;
-            isJulia = false;
+    if (fractalChoose == 'mandelbrotSet') {
+        xmin = -1.9, ymin = -1.5;
+        isJulia = false;
 
-        } else {
-            xmin = -1.4, ymin = -1.5;
-            isJulia = true;
-        }
+    } else {
+        xmin = -1.4, ymin = -1.5;
+        isJulia = true;
+    }
 
-        buildFractal();
-
-
+    buildFractal();
 
 
-        function zoom(event) {
-            var newScale = document.getElementById('scale').value;
-            var bodyRect = document.body.getBoundingClientRect(),
+
+
+    function zoom(event) {
+        var newScale = document.getElementById('scale').value;
+        var bodyRect = document.body.getBoundingClientRect(),
             elemRect = canvas.getBoundingClientRect(),
-            offset   = elemRect.top - bodyRect.top;
-            xmin += event.pageX/scale - 400/newScale/scale;
-            ymin += (event.pageY - offset)/scale - 400/newScale/scale;
-            scale*= newScale;
-            buildFractal();
+            offset = elemRect.top - bodyRect.top;
+        if (!isJulia) {
+            xmin += event.pageX / scale - 400 / newScale / scale;
+            ymin += (event.pageY - offset) / scale - 400 / newScale / scale;
+        } else {
+            xmin += event.pageX / scale - 400 / newScale / scale;
+            ymin += (event.pageY - offset) / scale - 800 / newScale / scale;
         }
+        scale *= newScale;
+        buildFractal();
+    }
 
-        function checkIfBelongsToMandelbrotSet(x, y){
-            var realComponentOfResult = x;
-            var imaginaryComponentOfResult = y;
-            var maxIterations = 100;
-            for (var i = 0; i < maxIterations; i++) {
-                var tempRealComponent = realComponentOfResult * realComponentOfResult
-                    - imaginaryComponentOfResult * imaginaryComponentOfResult;
-                var tempImaginaryComponent = 2 * realComponentOfResult * imaginaryComponentOfResult;
-                if(isJulia){
-                    realComponentOfResult = tempRealComponent + new Number(constForJuliaX);
-                    imaginaryComponentOfResult = tempImaginaryComponent + new Number(constForJuliaY);
-                }else{
-                    realComponentOfResult = tempRealComponent + x;
-                    imaginaryComponentOfResult = tempImaginaryComponent + y;
-                }
-            
-                // Return a number as a percentage
-                if (Math.abs(realComponentOfResult + imaginaryComponentOfResult) > 16)
-                    return (i / maxIterations * 100);
+    function checkIfBelongsToMandelbrotSet(x, y) {
+        var realComponentOfResult = x;
+        var imaginaryComponentOfResult = y;
+        var maxIterations = 100;
+        for (var i = 0; i < maxIterations; i++) {
+            var tempRealComponent = realComponentOfResult * realComponentOfResult
+                - imaginaryComponentOfResult * imaginaryComponentOfResult;
+            var tempImaginaryComponent = 2 * realComponentOfResult * imaginaryComponentOfResult;
+            if (isJulia) {
+                realComponentOfResult = tempRealComponent + new Number(constForJuliaX);
+                imaginaryComponentOfResult = tempImaginaryComponent + new Number(constForJuliaY);
+            } else {
+                realComponentOfResult = tempRealComponent + x;
+                imaginaryComponentOfResult = tempImaginaryComponent + y;
             }
-            return 0;   // Return zero if in set 
+
+            // Return a number as a percentage
+            if (Math.abs(realComponentOfResult + imaginaryComponentOfResult) > 16)
+                return (i / maxIterations * 100);
         }
+        return 0;   // Return zero if in set 
+    }
 
 
-        function buildFractal() {
-            for (x = 0; x < canvas.width; x++) {
-                for (y = 0; y < canvas.height; y++) {
-                    i = 0;
-                    cx = xmin + x / scale;
-                    cy = ymin + y / scale;
-                    var belongsToSet =
-                        checkIfBelongsToMandelbrotSet(x / scale + xmin,
-                            y / scale + ymin);
+    function buildFractal() {
+        for (x = 0; x < canvas.width; x++) {
+            for (y = 0; y < canvas.height; y++) {
+                i = 0;
+                cx = xmin + x / scale;
+                cy = ymin + y / scale;
+                var belongsToSet =
+                    checkIfBelongsToMandelbrotSet(x / scale + xmin,
+                        y / scale + ymin);
 
-                    if (belongsToSet == 0) {
-                        context .fillStyle = '#000';
-							context .fillRect(x, y, 1, 1); // Draw a black pixel
-						} else {
-							context .fillStyle = 'hsl(0, 100%, ' + belongsToSet + '%)';
-							context .fillRect(x, y, 1, 1); // Draw a colorful pixel
-						}
+                if (belongsToSet == 0) {
+                    context.fillStyle = '#000';
+                    context.fillRect(x, y, 1, 1); // Draw a black pixel
+                } else {
+                    context.fillStyle = 'hsl(0, 100%, ' + belongsToSet + '%)';
+                    context.fillRect(x, y, 1, 1); // Draw a colorful pixel
                 }
             }
         }
+    }
 }
 
-function getSliderInfo(){
+function getSliderInfo() {
     var sliderValue = document.getElementById("scale").value;
     document.getElementById("sliderValue").innerHTML = sliderValue;
 };
@@ -108,7 +113,7 @@ function addRadioButtonCheckEvent() {
 function checkRadioButton() {
     var fractalRadioButt = document.getElementsByName('fractalChoose');
     for (var i = 0; i < fractalRadioButt.length; i++) {
-        if (fractalRadioButt[i].checked){
+        if (fractalRadioButt[i].checked) {
             if (fractalRadioButt[i].value == 'mandelbrotSet') {
                 document.getElementById('constForJuliaX').hidden = true;
                 document.getElementById('constForJuliaY').hidden = true;
