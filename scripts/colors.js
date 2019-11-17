d881fe15a5ec9f8a572459ee10aa74548a4f69b2
LTania <tania.luchynska@gmail.com>
2019-11-17 22:59:12 +0200

    hsl - > hsv

--- scripts/colors.js
+++ scripts/colors.js
@@ -62,8 +62,8 @@ function doHSL(){
 // invert colors
 for (var i=0;i<imgData.data.length;i+=4)
   {
-  var hsl = rgbToHsl(imgData.data[i],imgData.data[i+1],imgData.data[i+2]);
-	  var rgbFromhsl =hslToRgb(hsl[0], hsl[1], hsl[2]);
+  var hsv = rgbToHsv(imgData.data[i],imgData.data[i+1],imgData.data[i+2]);
+	  var rgbFromhsl =hsvToRgb(hsv[0], hsv[1], hsv[2]);
 	  imgData.data[i]=rgbFromhsl[0];
 	  imgData.data[i+1]=rgbFromhsl[1];
 	  imgData.data[i+2]=rgbFromhsl[2];
@@ -71,50 +71,49 @@ for (var i=0;i<imgData.data.length;i+=4)
 ctx.putImageData(imgData,0,0);
 }
 
-function rgbToHsl(r, g, b){
-    r /= 255, g /= 255, b /= 255;
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
+function rgbToHsv(r, g, b) {
+  r /= 255, g /= 255, b /= 255;
 
-    if(max == min){
-        h = s = 0; // achromatic
-    }else{
-        var d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch(max){
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-        h /= 6;
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, v = max;
+
+  var d = max - min;
+  s = max == 0 ? 0 : d / max;
+
+  if (max == min) {
+    h = 0; // achromatic
+  } else {
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
     }
 
-    return [h, s, l];
+    h /= 6;
+  }
+
+  return [ h, s, v ];
 }
 
-function hslToRgb(h, s, l){
-    var r, g, b;
+function hsvToRgb(h, s, v) {
+  var r, g, b;
 
-    if(s == 0){
-        r = g = b = l; // achromatic
-    }else{
-        var hue2rgb = function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
+  var i = Math.floor(h * 6);
+  var f = h * 6 - i;
+  var p = v * (1 - s);
+  var q = v * (1 - f * s);
+  var t = v * (1 - (1 - f) * s);
 
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
-    }
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
 
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+  return [ r * 255, g * 255, b * 255 ];
 }
 
 
