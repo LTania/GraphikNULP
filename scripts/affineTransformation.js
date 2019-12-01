91e1ffd9b4507405eeb75857f9c94fa8ddd1fd3a
Bohdan <iabodia88@gmail.com>
2019-12-02 00:02:03 +0200

    Add square building

--- scripts/affineTransformation.js
+++ scripts/affineTransformation.js
@@ -1,10 +1,69 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', init);
+
+  function createGrid () {
+    // draw a line every *step* pixels
+    const step = 80;
+
+    // our end points
+    console.log(canvas.width, canvas.height);
+    const width = canvas.width;
+    const height = canvas.height;
+
+    const startX = -100;
+
+    const startY = -100;
+
+    // set our styles
+    ctx.save();
+    ctx.strokeStyle = 'gray'; // line colors
+    ctx.fillStyle = 'black'; // text color
+    ctx.font = '8px Monospace';
+    ctx.lineWidth = 0.4;
+
+    // draw vertical from X to Height
+    for (let x = 0; x < width; x += step) {
+      // draw vertical line
+      ctx.beginPath();
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, height);
+      ctx.stroke();
+
+      // draw text
+      ctx.fillText((x + startX)/20, x, 10);
+    }
+
+    // draw horizontal from Y to Width
+    for (let y = 0; y < height; y += step) {
+      // draw horizontal line
+      ctx.beginPath();
+      ctx.moveTo(0, y);
+      ctx.lineTo(width, y);
+      ctx.stroke();
+
+      // draw text
+      ctx.fillText((height - y + startY)/20, 0, y);
+    }
+
+    // restore the styles from before this function was called
+    ctx.restore();
+  }
+
+  function init () {
+    // set our config variables
+    canvas = document.getElementById('canvas-affin');
+    ctx = canvas.getContext('2d');
+
+    createGrid();
+  }
+
+  var xA, yA, xB, yB, xC, yC, xD, yD;
+
 function findDots() {
 
-    var xA = Number(document.getElementById('xA').value), 
-        yA = Number(document.getElementById('yA').value);
-    var xC = Number(document.getElementById('xC').value),
-        yC = Number(document.getElementById('yC').value);
+    xA = Number(document.getElementById('xA').value);
+    yA = Number(document.getElementById('yA').value);
+    xC = Number(document.getElementById('xC').value);
+    yC = Number(document.getElementById('yC').value);
 
     var deltaX = xC - xA;
     var deltaY = yC - yA;
@@ -12,74 +71,30 @@ function findDots() {
     var xO = (xC+xA)/2;
     var yO = (yC+yA)/2;
 
-    var xB = xO - deltaY/2;
-    var yB = yO + deltaX/2;
+    xB = xO - deltaY/2;
+    yB = yO + deltaX/2;
 
-    var xD = xO + deltaY/2;
-    var yD = yO - deltaX/2;
+    xD = xO + deltaY/2;
+    yD = yO - deltaX/2;
 
     console.log(xB, yB, xD, yD);
 }
 
-(function () {
-    let canvas, ctx;
-  
-    // draws a grid
-    function createGrid () {
-      // draw a line every *step* pixels
-      const step = 30;
-  
-      // our end points
-      console.log(canvas.width, canvas.height);
-      const width = canvas.width;
-      const height = canvas.height;
+function drowSquare(){
+    findDots();
+    canvas = document.getElementById('canvas-affin');
+    ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    init();
+    ctx.moveTo(100 + xA*20, 500 - 20*yA);
+    ctx.lineTo(100 + xB*20, 500 - 20*yB);
+    ctx.moveTo(100 + xB*20, 500 - 20*yB);
+    ctx.lineTo(100 + xC*20, 500 - 20*yC);
+    ctx.moveTo(100 + xC*20, 500 - 20*yC);
+    ctx.lineTo(100 + xD*20, 500 - 20*yD);
+    ctx.moveTo(100 + xD*20, 500 - 20*yD);
+    ctx.lineTo(100 + xA*20, 500 - 20*yA);
+    ctx.stroke();
 
-      const startX = -100;
-
-      const startY = -100;
-  
-      // set our styles
-      ctx.save();
-      ctx.strokeStyle = 'gray'; // line colors
-      ctx.fillStyle = 'black'; // text color
-      ctx.font = '8px Monospace';
-      ctx.lineWidth = 0.2;
-  
-      // draw vertical from X to Height
-      for (let x = 0; x < width; x += step) {
-        // draw vertical line
-        ctx.beginPath();
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, height);
-        ctx.stroke();
-  
-        // draw text
-        ctx.fillText(x + startX, x, 10);
-      }
-  
-      // draw horizontal from Y to Width
-      for (let y = 0; y < height; y += step) {
-        // draw horizontal line
-        ctx.beginPath();
-        ctx.moveTo(0, y);
-        ctx.lineTo(width, y);
-        ctx.stroke();
-  
-        // draw text
-        ctx.fillText(height - y + startY, 0, y);
-      }
-  
-      // restore the styles from before this function was called
-      ctx.restore();
-    }
-  
-    function init () {
-      // set our config variables
-      canvas = document.getElementById('canvas-affin');
-      ctx = canvas.getContext('2d');
-  
-      createGrid();
-    }
-  
-    document.addEventListener('DOMContentLoaded', init);
-  })()
+}
