414783df638f1d0972e9e1f42645f7f4bae23363
LTania <tania.luchynska@gmail.com>
2019-12-01 20:42:28 +0200

    scroll

--- scripts/header.js
+++ scripts/header.js
@@ -1,35 +1,17 @@
 document.addEventListener("DOMContentLoaded",function(){
 	
-	function scrollHome() {
-		let goal = document.getElementById('home') ;
-		goal.scrollIntoView({block:"start"});
-	}
-
-	function scrollFractals() {
-		let goal = document.getElementById('fractals') ;
-		goal.scrollIntoView();
-	}
-	function scrollColors() {
-		let goal = document.getElementById('colors') ;
-		goal.scrollIntoView({block:"start"});
-	}
-	function scrollAffin() {
-		let goal = document.getElementById('affin') ;
-		goal.scrollIntoView({block:"start"});
-	}
-	function scrollAbout() {
-		let goal = document.getElementById('info');
-		goal.scrollIntoView({block:"start"});
-	}
-	function scrollTeam() {
-		let goal = document.getElementById('team');
-		goal.scrollIntoView({block:"start"});
+	function scroolToIndex(index) {
+		return function(event) {
+			event.stopPropagation();
+			let goal = document.getElementById(index) ;
+			setTimeout(() => goal.scrollIntoView({behavior: "smooth"}), 10);	
+		};
 	}
 	
-	document.getElementById("team-nav-btn").addEventListener("click",scrollTeam);	
-	document.getElementById("affine-nav-btn").addEventListener("click",scrollAffin);
-	document.getElementById("colors-nav-btn").addEventListener("click",scrollColors);
-	document.getElementById("fractals-nav-btn").addEventListener("click",scrollFractals);	
-	document.getElementById("home-nav-btn").addEventListener("click",scrollHome);
-	document.getElementById("about-nav-btn").addEventListener("click",scrollAbout);
+	document.getElementById("team-nav-btn").addEventListener("click", scroolToIndex("team"));	
+	document.getElementById("affine-nav-btn").addEventListener("click", scroolToIndex("affin"));
+	document.getElementById("colors-nav-btn").addEventListener("click", scroolToIndex("colors"));
+	document.getElementById("fractals-nav-btn").addEventListener("click", scroolToIndex("fractals"));	
+	document.getElementById("home-nav-btn").addEventListener("click", scroolToIndex("home"));
+	document.getElementById("about-nav-btn").addEventListener("click", scroolToIndex("info"));
 })
