864436a7172f12b03713acfd1b1a7dae422b2da8
Bohdan <iabodia88@gmail.com>
2019-12-09 22:44:21 +0200

    Add dovidka

---
+++ scripts/chooseText.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener('DOMContentLoaded', chooseTextFract);
+
+function chooseTextFract(){
+    var elem = document.getElementById("infoTextFractals");
+    var elem1 = document.getElementById("infoTextColour");
+    var elem2 = document.getElementById("infoTextAffine");
+    elem.style.display = 'inline';
+    elem1.style.display = 'none';
+    elem2.style.display = 'none';
+
+}
+
+function chooseTextColor(){
+    var elem = document.getElementById("infoTextFractals");
+    var elem1 = document.getElementById("infoTextColour");
+    var elem2 = document.getElementById("infoTextAffine");
+    elem.style.display = 'none';
+    elem1.style.display = 'flex';
+    elem2.style.display = 'none';
+}
+
+function chooseTextAffine(){
+    var elem = document.getElementById("infoTextFractals");
+    var elem1 = document.getElementById("infoTextColour");
+    var elem2 = document.getElementById("infoTextAffine");
+    elem.style.display = 'none';
+    elem1.style.display = 'none';
+    elem2.style.display = 'inline';
+}
